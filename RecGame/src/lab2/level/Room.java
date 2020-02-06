be7c030d43ab600fe0c57d5bb7b40bd0c78194ed
abbos <abbos@100.74.130.187>
2020-02-06 19:44:46 +0100

    Fix variables and add variable color. Add print to constructor Room

--- RecGame/src/lab2/level/Room.java
+++ RecGame/src/lab2/level/Room.java
@@ -5,14 +5,14 @@ import java.awt.Color;
 
 
 public class Room { 
-	public Room North = null;
-	public Room East = null;
-	public Room South = null;
-	public Room West = null;
-	
+	Room north = null;
+	Room east = null;
+	Room south = null;
+	Room west = null;
+	Color color = null;
 	
 	public Room(int dx, int dy, Color color) {
-		
+		System.out.println("The rooms dimention is " + dx +"," + dy + " and the floor has color" + color);
 	}
 
 	public void connectNorthTo(Room r) {
