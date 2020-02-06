4675dca924f384594e697644af2a9a0679817347
abbos <abbos@100.74.130.187>
2020-02-06 23:03:48 +0100

    In class Room we created int x, y, dx, dy.
    Add check up function to place in Level class.

--- RecGame/src/lab2/level/Level.java
+++ RecGame/src/lab2/level/Level.java
@@ -1,6 +1,7 @@
 
 package lab2.level;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 
@@ -8,7 +9,22 @@ public class Level extends Observable {
 	
 	
 	public boolean place(Room r, int x, int y)  {
+		ArrayList<Room> Length = new ArrayList<Room>();
 		
+		for(int i = 0; i <= Length.size();i++) {
+			int startX = Length.get(i).x;
+			int endX = startX + Length.get(i).dx;
+			int startY = Length.get(i).y;
+			int endY = startY + Length.get(i).dy;
+			
+			if ((x+r.dx) >= startX || x<=endX || (y + r.dy) >= startY || y<=endY) {
+				return false;
+			}
+		}
+		r.x = x;
+		r.y = y;
+		Length.add(r);
+		return true;
 	}
 	
 	public void firstLocation(Room r) {
--- RecGame/src/lab2/level/Room.java
+++ RecGame/src/lab2/level/Room.java
@@ -4,7 +4,8 @@ package lab2.level;
 import java.awt.Color;
 
 
-public class Room { 
+public class Room {
+	int x = 0, y = 0, dx = 0, dy = 0;
 	Room north = null;
 	Room east = null;
 	Room south = null;
@@ -12,19 +13,22 @@ public class Room {
 	Color color = null;
 	
 	public Room(int dx, int dy, Color color) {
+		this.dy = dy;
+		this.dx = dx;
+		this.color = color;
 		System.out.println("The rooms dimention is " + dx +"," + dy + " and the floor has color" + color);
 	}
 
 	public void connectNorthTo(Room r) {
-		
+		north = r;
 	}
 	public void connectEastTo(Room r) {
-		
+		east = r;
 	}
 	public void connectSouthTo(Room r) {
-		
+		south = r;
 	}
 	public void connectWestTo(Room r) {
-		
+		west = r;
 	}
 }
