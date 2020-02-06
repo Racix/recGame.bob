b8c94ff3b90953978ad8e3bba8c3d24539197db3
abbos <abbos@100.74.130.187>
2020-02-06 20:11:58 +0100

    Rooms

--- RecGame/src/lab2/Driver.java
+++ RecGame/src/lab2/Driver.java
@@ -1,5 +1,7 @@
 package lab2;
 
+import java.awt.Color;
+
 import lab2.level.Level;
 import lab2.level.LevelGUI;
 import lab2.level.Room;
@@ -8,6 +10,9 @@ public class Driver {
 
 	public void run() {
 		System.out.println("This is a print-out from the driver.");
+		Room roomOne = new Room(100,200, Color.red);
+		Room roomSecound = new Room(100,200, Color.blue);
+		Room roomThird = new Room(100,200, Color.green);
 		
 	}
 	
